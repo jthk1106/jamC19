efa92a53cca8fbd3b9895bb733765e5c423c12a9
Jay <thk116@gmail.com>
2020-05-03 23:08:24 -0700

    ready to push to heroku

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,52 @@ import C19 from "../components/c19stats"
 
 const date = moment().format('MMMM Do YYYY')
 
+const sendMail = () => {
+  var proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+  var url = 'https://enigmatic-bastion-43812.herokuapp.com/contact';
+  var formData = new FormData();
+  formData.append("name", "Tester");
+  formData.append("email", "testermail@123.io");
+  formData.append("subject", "Testing");
+  formData.append("message", "This is a test mail");
+  
+  fetch(proxyUrl + url, {
+    method: 'POST',
+    headers: {
+      'Accept': 'text/plain',
+      'Content-Type': 'text/plain'
+    },
+    body: formData
+  })
+    .then(response => response.text())
+    .then(contents => console.log(contents))
+    .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
+
+  // fetch(url, {
+  //   method: 'POST',
+  //   mode: 'no-cors',
+  //   headers: {'Accept': 'text/plain', 'Content-Type': 'text/plain'},
+  //   body: formData
+  // }).then(res => res.json())
+  // .catch(error => console.error('Error:', error))
+  // .then(response => console.log('Success:', response));
+
+  // axios({
+  //   method: 'POST',
+  //   url: proxyUrl + targetUrl,
+  //   data: formData,
+  //   mode: 'no-cors',
+  //   headers: {
+  //     Accept: 'text/plain',
+  //     'Content-Type': 'text/plain',
+  //   },
+  //   // withCredentials: false,
+  //   // credentials: 'same-origin',
+  // })
+  //   .then(res => console.log(res))
+  //   .catch(err => console.log(err))
+}
+
 const IndexPage = () => (
   // <Layout>
   <div>
@@ -18,11 +64,11 @@ const IndexPage = () => (
     <div className="jumbotron jumbotron-fluid text-center">
         <div className="container">
             <div className="row justify-content-center">
-                <h1 className="display-4 text-center title-left glow">covid19</h1>
-                <h1 className="display-4 text-center title-right glow">코로나19</h1>
+                <h1 className="display-4 text-center title-left glow">{`covid19 · 코로나19`}</h1>
+                {/* <h1 className="display-4 text-center title-right glow">코로나19</h1> */}
             </div>
             <p className="lead title-msg">Statistics monitor on covid19 with data sourced from Worldometer.</p>
-            <p>(Data is aggregated and continuously updated so metrics will refresh and appear most complete towards end of day)</p>
+            <p className="title-msg-sm">(Data is aggregated with continuous updating and metrics will refresh to appear most complete towards end of day)</p>
         </div>
     </div>
     <div className="container">
@@ -33,8 +79,11 @@ const IndexPage = () => (
             </div>
         </div>
         <C19></C19>
+        <div className="row justify-content-center">
+          <button className="btn btn-success" onClick={sendMail}>Email</button>
+        </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    {/* <Link to="/page-2/">Go to page 2</Link> */}
     {/* </Layout> */}
   </div>
 )
